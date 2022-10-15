e023ccf8233de358d952dd4d47219b195bb6e35e
rosim24 <rosim24@gmail.com>
2022-10-15 14:10:59 -0400

    order by price

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,4 +1,4 @@
-//import actions
+import { ORDER_BY_PRICE } from "./action";
 
 const initialState = {
     categories: [],
@@ -8,6 +8,24 @@ const reducer = (state = initialState, action) =>{
     switch(action.type) {
         case "ALGO":{
             break;
+        }
+        case ORDER_BY_PRICE:{
+            let ordered = [];
+            if(action.payload === "asc"){
+                ordered = state.productaux.sort(function(a,b){
+                    if(a.price > b.price) return 1
+                    else if(b.price > a.price) return -1
+                    else return 0
+                  })
+                  return {...state, productaux: ordered}
+            }else if(action.payload === "desc"){
+                ordered = state.productaux.sort(function(a,b){
+                  if(a.price > b.price) return -1
+                  else if(b.price > a.price) return 1
+                  else return 0
+                })
+                return {...state, productaux: ordered}
+              }
         }
         default: return state;
     }
