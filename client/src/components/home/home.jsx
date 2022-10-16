afdc5e21ac8116e35c9ede63ed099d2459c6d9e6
Luis Acosta <acosta1395@hotmail.com>
2022-10-16 17:11:10 -0300

    hice un loading para el home

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -2,6 +2,7 @@ import React,{useEffect} from "react";
 import Card from '../card/card'
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllProducts } from '../../redux/actions/get_products';
+import Loading from "../loading/loading";
 
 export default function Home(){
 
@@ -11,10 +12,10 @@ export default function Home(){
     useEffect(() => {  // Didmount and DidUpdate controlled
         dispatch(getAllProducts());
     },[dispatch]) 
-
+    console.log(products)
     return(
         <div>
-            {products.map(a => a.stock === 0 ? null : <Card name={a.name} image={a.image} price={a.price}/>)}
+            {products[0] && products[0].price ? products.map(a => a.stock === 0 ? null : <Card name={a.name} image={a.image} price={a.price}/>) : <Loading/>}
         </div>
     )
 }
