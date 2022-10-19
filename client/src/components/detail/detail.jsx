6c444877d8e936be0430b6b1c03d0cca2bb1030f
Luis Acosta <acosta1395@hotmail.com>
2022-10-19 18:16:26 -0300

    nueva seccion: similares

--- client/src/components/detail/detail.jsx
+++ client/src/components/detail/detail.jsx
@@ -6,62 +6,66 @@ import {useEffect} from "react";
 import {getProductDetails} from '../../redux/actions/get_product_details';
 import {cleanProductState} from '../../redux/actions/clean_product_state';
 import StarRoundedIcon from '@mui/icons-material/StarRounded';
+import RelatedProducts from "./relatedProducts";
 
 function Detail(props) {
-  const {id} = props.match.params;
-  const dispatch = useDispatch();
+    const {id} = props.match.params;
+    const dispatch = useDispatch();
     
-  useEffect(()=> {
-       dispatch(getProductDetails(id));
-       return(() => {
-           dispatch(cleanProductState({}))
-       })
-   }, [dispatch, id])
+    useEffect(()=> {
+        dispatch(getProductDetails(id));
+        return(() => {
+            dispatch(cleanProductState({}))
+        })
+    }, [dispatch, id])
 
-   const detail = useSelector((state) => state.productdetail)
-   
-  let stars = [];
-  for (let i = 0; i < Math.ceil(detail.score); i++) {
-    stars.push(<StarRoundedIcon />)
-    
-  }
-
-  return (<div className="detailContainer"> 
-            <div className="detailContainerArticles">
-                    <div className="detailLeft">
-                        <h1 className="detailTitle">{detail.name}</h1>
-                        <span className="detailScore">{stars}</span>
-                        <h2 className="detailPrice"> $ {detail.price}</h2>
-                        <p className="detailDescription">{detail.description}</p>
-                    <div className="detailButton">
-                        <span className="buttonMargin"><Button variant="contained" >Comprar</Button></span>
-                        <Button variant="contained">Agregar al Carrito</Button>
+    const detail = useSelector((state) => state.productdetail)
+    let stars = [];
+    for (let i = 0; i < Math.ceil(detail.score); i++) {
+        stars.push(<StarRoundedIcon />)
+    }
+    detail.categories && console.log(detail.categories[0])
+    return (
+            <div className="detailContainer"> 
+                <div className="detailContainerArticles">
+                        <div className="detailLeft">
+                            <h1 className="detailTitle">{detail.name}</h1>
+                            <span className="detailScore">{stars}</span>
+                            <h2 className="detailPrice"> $ {detail.price}</h2>
+                            <p className="detailDescription">{detail.description}</p>
+                        <div className="detailButton">
+                            <span className="buttonMargin"><Button variant="contained" >Comprar</Button></span>
+                            <Button variant="contained">Agregar al Carrito</Button>
+                        </div>
                     </div>
+                    <div className="detailImagen">
+                        <img src={detail.image} alt="productos" />
+                    </div>
+                </div>
+                <div className="detailContainerOpinion detailMargin">
+                    <h1 className="opinionTitle">Opiniones</h1>
+                    <div className="opinionContainer">
+                        {detail.opiniones && detail.opiniones.map((e) => {
+                            let starsOpinion = [];
+                            for (let i = 0; i < Math.ceil(e.score); i++) {
+                                starsOpinion.push(<StarRoundedIcon />)
+                            }
+                            return (
+                                <div >
+                                    <span>{starsOpinion}</span>
+                                    <p className="detailDescription">{e.coment}</p>
+                                </div>
+                            )
+                        })}
+                    </div>  
+                    <Button variant="contained">Dar tu Opinion</Button>
+                </div>
+                <div>
+                    <RelatedProducts id={detail.id} categorie={detail.categories ? detail.categories[0] : null}/>
+                </div>
             </div>
-                    <div className="detailImagen"><img src={detail.image} alt="productos" /></div>
-            </div>
-            <div className="detailContainerOpinion detailMargin">
-            <h1 className="opinionTitle">Opiniones</h1>
-            <div className="opinionContainer">
-            {detail.opiniones && detail.opiniones.map((e) => {
-                let starsOpinion = [];
-                for (let i = 0; i < Math.ceil(e.score); i++) {
-                    starsOpinion.push(<StarRoundedIcon />)
-                    
-                  }
-                return (
-                <div >
-                    
-                    <span>{starsOpinion}</span>
-                    <p className="detailDescription">{e.coment}</p>
-                </div>)
-            })}
-            </div>  
-            <Button variant="contained">Dar tu Opinion</Button>
-        </div>
-           
-        </div>
-)}
+    )
+}
 
 export default Detail;
 
---
+++ client/src/components/detail/relatedProducts.jsx
@@ -0,0 +1,28 @@
+import React,{useEffect} from "react";
+import Card from '../card/card'
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllProducts } from "../../redux/actions/get_products";
+import Detail from "./detail";
+
+export default function RelatedProducts(props){
+    console.log(props.categorie)
+    const dispatch = useDispatch();
+    const products = useSelector( state => state.products)
+
+    useEffect(() => {  // Didmount and DidUpdate controlled
+        dispatch(getAllProducts());
+    },[dispatch]) 
+
+    const relatedProducts = products.filter(e => e.categories.includes(props.categorie)).filter(e => e.id !== props.id).slice(0,4)
+
+    return(
+        relatedProducts.length > 1 && <div className="categorieTable">
+            <div className="productsByCategorie">
+                <h2 className="categorieTitle">Tambien te puede interesar</h2>
+                <div className="homeTable">
+                    { relatedProducts.map(a => a.stock === 0 ? null : <Card id={a.id} name={a.name} image={a.image} price={a.price} score={a.score}/>) }
+                </div>
+            </div>
+        </div>
+    )
+}
