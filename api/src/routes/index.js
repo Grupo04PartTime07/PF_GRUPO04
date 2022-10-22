1071d5a558e8dfaa6a071d636c9d591426a7d932
rosim24 <rosim24@gmail.com>
2022-10-22 12:58:41 -0400

    localStorageFix4Guest

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const mwcategories = require('./categories')
 const mwproducts = require('./products')
 const mwbrands = require('./brands') 
-const mwcart = require('./Cart')
+//const mwcart = require('./Cart')
 const mwinventory = require('./inventory')
 const mwcheckout = require('./checkout') 
 //const mwserver = require('./server') 
@@ -13,7 +13,7 @@ const router = Router();
 router.use('/categories', mwcategories);
 router.use('/products', mwproducts);
 router.use('/brands', mwbrands);
-router.use('/cart', mwcart)
+//router.use('/cart', mwcart)
 router.use('/inventory', mwinventory)
 router.use('/checkout', mwcheckout);
 //router.use('/server', mwserver);
