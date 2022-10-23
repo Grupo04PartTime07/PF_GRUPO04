0594fcafa9be8ef7a6bb6d13f85d792cfb5c6d74
Gaston1980 <gastonfrissiones@yahoo.com.ar>
2022-10-23 15:05:10 -0300

    fix(client/shoppingCart):pick up option added to delivery method in cart

--- client/src/components/shoppingCartBig/shoppingCart.jsx
+++ client/src/components/shoppingCartBig/shoppingCart.jsx
@@ -68,10 +68,11 @@ function handleDisabled(){
                     <select className={styles.shippingPrice} name="shipping-costs"id="shipping-costs" defaultValue=""
                     onChange={(e) => setShipping(e.target.value) }
                     >
-                    <option disabled value="" >Elije una franja horaria</option>
-                    <option value="299">$299 (8hs a 21hs - CABA)</option>
-                    <option value="349">$349 (12hs a 16hs - CABA)</option>
-                    <option value="399">$399 (18hs a 21hs - CABA)</option>
+                    <option disabled value="" >Elije una opcion de entrega:</option>
+                    <option value="0">$0 (Retiro en Tienda: 10hs a 20hs - Av. Cordoba 1940, CABA)</option>
+                    <option value="299">$299 (Envio a domicilio: 8hs a 21hs - CABA)</option>
+                    <option value="349">$349 (Envio a domicilio: 12hs a 16hs - CABA)</option>
+                    <option value="399">$399 (Envio a domicilio: 18hs a 21hs - CABA)</option>
                     </select>
                 </div>
                 <div className={styles.divCantProductos}>
