9cb967f9b3581176662d37dc6200557e686ac5dc
Luis Acosta <acosta1395@hotmail.com>
2022-10-23 19:04:21 -0300

    Apliqué filtros a los productos por marcas

--- client/src/components/Brands/brand_products.jsx
+++ client/src/components/Brands/brand_products.jsx
@@ -1,8 +1,11 @@
-import React,{useEffect} from "react";
+import React,{useEffect, useState} from "react";
 import Card from '../card/card'
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import { filterByBrand } from "../../redux/actions/filter_by_brand";
 import { cleanProducts } from "../../redux/actions/clean_products";
+import { orderByPrice } from '../../redux/actions/order_price';
+import { orderByRate } from "../../redux/actions/order_rate";
 import Loading from "../loading/loading";
 import './brand_products.css'
 
@@ -10,6 +13,7 @@ export default function Brand(props){
     
     const dispatch = useDispatch();
     const productsaux = useSelector( state => state.productsaux)
+    const [ order, setOrder ] = useState('')
 
     useEffect(() => {  // Didmount and DidUpdate controlled
         window.scrollTo(0, 0)
@@ -19,11 +23,50 @@ export default function Brand(props){
         })
     },[dispatch]) 
 
+    function handlePrice(e){
+        dispatch(orderByPrice(e.target.value))
+        setOrder(e.target.value)
+    }
+
+    function handleRate(e){
+        dispatch(orderByRate(e.target.value))
+        setOrder(e.target.value)
+    }
+
     return(
-        productsaux[0] && productsaux[0].price ? <div>
-            <h2 className="categorieTitle">{props.history.location.state}</h2>
-            <div className="homeTable">
-                { productsaux.map(a => a.stock === 0 ? null : <Card id={a.id} name={a.name} image={a.image} price={a.price} score={a.score}/>) }
+        productsaux[0] && productsaux[0].price ? <div className="categorieTable">
+            <div className="categorieFilters">
+                <Link className="linkToBack" to='/Marcas'>
+                    <button className="linkToBack">
+                        Volver
+                    </button>
+                </Link>
+                <div>
+                    Filtrar por:
+                
+                    <div className="categorieOptions">
+                        <div className="priceFilter">
+                            Precio
+                        </div>
+                        <div className="buttonsTable">
+                            <button id='asc' className="filterButtons" value='asc' onClick={(e) => handlePrice(e)}>Menor a mayor precio</button>
+                            <button id='desc' className="filterButtons" value='desc' onClick={(e) => handlePrice(e)}>Mayor a menor precio</button>
+                        </div>
+                        <div className="ratingFilter">
+                            Valoración
+                        </div>
+                        <div className="buttonsTable">
+                            <button className="ratingButtons" value='good' onClick={(e) => handleRate(e)}>Más valorado</button>
+                            <button className="ratingButtons" value='bad' onClick={(e) => handleRate(e)}>Menos valorado</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="productsByCategorie">
+                <h2 className="categorieTitle">{props.history.location.state}</h2>
+                <div className="homeTable">
+                    { productsaux.map(a => a.stock === 0 ? null : <Card id={a.id} name={a.name} image={a.image} price={a.price} score={a.score}/>) }
+                </div>
             </div>
         </div> : <Loading/>
     )
