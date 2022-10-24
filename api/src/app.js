d5ab2b76f007b508a7f61897547db3a8111a5704
EnzoMaidana <enzomaidana1995@gmail.com>
2022-10-24 20:26:43 -0300

    Deploy controlado

--- api/src/app.js
+++ api/src/app.js
@@ -37,7 +37,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', `${FRONT_URL}`); 
+  res.header('Access-Control-Allow-Origin', `https://pg-henrymarket.vercel.app`); 
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
