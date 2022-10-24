c8bbc0e3c80cbeeafd42fb400afe6935cf0c86f3
bbroscheit <bernardo.broscheit@gmail.com>
2022-10-24 16:17:55 -0300

    faltabaun div

--- client/src/components/detail/detail.jsx
+++ client/src/components/detail/detail.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import './detail.css';
 import { Button } from "@mui/material";
-import {useDispatch, useSelector} from "react-redux";
-import {useEffect} from "react";
-import {getProductDetails} from '../../redux/actions/get_product_details';
-import {cleanProductState} from '../../redux/actions/clean_product_state';
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getProductDetails } from '../../redux/actions/get_product_details';
+import { cleanProductState } from '../../redux/actions/clean_product_state';
 import StarRoundedIcon from '@mui/icons-material/StarRounded';
 import RelatedProducts from "./relatedProducts";
 import ScoreForm from "./scoreForms";
 import Loading from "../loading/loading";
 import { addToCart } from '../../redux/actions/add_to_cart';
-import {Link} from "react-router-dom"
-import {useAuth0} from '@auth0/auth0-react';
+import { Link } from "react-router-dom"
+import { useAuth0 } from '@auth0/auth0-react';
 
 function Detail(props) {
     const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
-  
-    const {id} = props.match.params;
+
+    const { id } = props.match.params;
     const dispatch = useDispatch();
     const [displayForm, setDisplay] = React.useState(false);
-    
-    useEffect(()=> {
+
+    useEffect(() => {
         window.scrollTo(0, 0)
         dispatch(getProductDetails(id));
-        return(() => {
+        return (() => {
             dispatch(cleanProductState({}))
         })
     }, [dispatch, id])
 
-    function formDisplay(){
+    function formDisplay() {
         setDisplay(!displayForm)
     }
 
@@ -40,53 +40,54 @@ function Detail(props) {
 
     return (
 
-            typeof detail.id === 'number' ? 
+        typeof detail.id === 'number' ?
             <div className="detailContainer">
 
-                    <div className="detailContainerArticles">
-                        <div className="detailLeft"> {/*Agregar una estiqueta de producto no disponible condicionada al stock*/}
-                            <h1 className="detailTitle">{detail.name}</h1>
-                            <span className="detailScore">{stars}</span>
-                            <h2 className="detailPrice"> $ {detail.price}</h2>
-                            <p className="detailDescription">{detail.description}</p>
-                        </div>    
-                        <div className="detailButton">
-                            <Link to="/shoppingCart" style={{textDecoration:"none"}}>
-                            <span className="buttonMargin"><Button onClick={()=> dispatch(addToCart({id: detail.id, name: detail.name, image: detail.image, price: detail.price, quantity: 1})) } variant="contained" >Comprar</Button></span>
-                            </Link>
-                            <Button onClick={()=> dispatch(addToCart({id: detail.id, name: detail.name, image: detail.image, price: detail.price, quantity: 1})) } variant="contained">Agregar al Carrito</Button>
-                        </div>
-                   
+                <div className="detailContainerArticles">
+                    <div>
+                    <div className="detailLeft"> {/*Agregar una estiqueta de producto no disponible condicionada al stock*/}
+                        <h1 className="detailTitle">{detail.name}</h1>
+                        <span className="detailScore">{stars}</span>
+                        <h2 className="detailPrice"> $ {detail.price}</h2>
+                        <p className="detailDescription">{detail.description}</p>
+                    </div>
+                    <div className="detailButton">
+                        <Link to="/shoppingCart" style={{ textDecoration: "none" }}>
+                            <span className="buttonMargin"><Button onClick={() => dispatch(addToCart({ id: detail.id, name: detail.name, image: detail.image, price: detail.price, quantity: 1 }))} variant="contained" >Comprar</Button></span>
+                        </Link>
+                        <Button onClick={() => dispatch(addToCart({ id: detail.id, name: detail.name, image: detail.image, price: detail.price, quantity: 1 }))} variant="contained">Agregar al Carrito</Button>
+                    </div>
+                    </div>
                     <div className="detailImagen">
                         <img src={detail.image} alt="productos" />
                     </div>
                 </div>
-            
+
                 <div className="detailContainerOpinion detailMargin">
                     <h1 className="opinionTitle">Opiniones</h1>
                     <div className="opinionCard">
-                      <div className="opinionContainer">
-                          {detail.opiniones && detail.opiniones.map((e) => {
-                            let starsOpinion = [];
-                            for (let i = 0; i < Math.ceil(e.score); i++) {
-                                starsOpinion.push(<StarRoundedIcon />)
-                            }
-                            return (
-                                <div >
-                                    <span>{starsOpinion}</span>
-                                    <p className="detailDescription">{e.coment}</p>
-                                </div>
-                            )
-                          })}
-                      </div>
-                          {displayForm && <ScoreForm id={id} formDisplay={formDisplay} />}
+                        <div className="opinionContainer">
+                            {detail.opiniones && detail.opiniones.map((e) => {
+                                let starsOpinion = [];
+                                for (let i = 0; i < Math.ceil(e.score); i++) {
+                                    starsOpinion.push(<StarRoundedIcon />)
+                                }
+                                return (
+                                    <div >
+                                        <span>{starsOpinion}</span>
+                                        <p className="detailDescription">{e.coment}</p>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        {displayForm && <ScoreForm id={id} formDisplay={formDisplay} />}
                     </div>
-                      {isAuthenticated && <Button variant="contained" onClick={() =>formDisplay()}>Dar tu Opinion</Button>}
-               </div>
-               <div>
-                    <RelatedProducts id={detail.id} categorie={detail.categories ? detail.categories[0] : null}/>
-               </div>
-        </div> : <Loading/>
+                    {isAuthenticated && <Button variant="contained" onClick={() => formDisplay()}>Dar tu Opinion</Button>}
+                </div>
+                <div>
+                    <RelatedProducts id={detail.id} categorie={detail.categories ? detail.categories[0] : null} />
+                </div>
+            </div> : <Loading />
     )
 }
 
