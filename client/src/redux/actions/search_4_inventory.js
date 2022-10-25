3335ced964c027a14bc1307377e719d7690887ab
EnzoMaidana <enzomaidana1995@gmail.com>
2022-10-25 17:37:04 -0300

    changes deploy

--- client/src/redux/actions/search_4_inventory.js
+++ client/src/redux/actions/search_4_inventory.js
@@ -5,7 +5,7 @@ const { BACK_URL = 'http://localhost:3001' } = process.env
 export function searchForInventory(name) {
     if(name !== ""){
     return async function (dispatch) {
-        var json = await axios(`${BACK_URL}/products?name=` + name);
+        var json = await axios(`https://pfproduction-production.up.railway.app/products?name=` + name);
         return dispatch({
             type: SEARCH_FOR_INVENTORY,
             payload: json.data
