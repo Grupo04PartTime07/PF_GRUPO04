3da217bc6c284822aac67d5b551ee09d26feaa30
Gaston1980 <gastonfrissiones@yahoo.com.ar>
2022-10-31 01:49:39 -0300

    feat(client/orderList):new component and new features

---
+++ client/src/redux/actions/add_order_to_cart.js
@@ -0,0 +1,7 @@
+export const ADD_ORDER_TO_CART = "ADD_ORDER_TO_CART";
+    
+export const addOrderToCart = (payload) => {
+    return {
+        type: ADD_ORDER_TO_CART,
+        payload
+        }} 
