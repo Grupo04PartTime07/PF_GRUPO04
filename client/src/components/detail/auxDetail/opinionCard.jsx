5691a0301d288b1d1ad831746663d061d430f638
Luis Acosta <acosta1395@hotmail.com>
2022-11-04 02:29:49 -0300

    limpieza de codigo en opinionCard

--- client/src/components/detail/auxDetail/opinionCard.jsx
+++ client/src/components/detail/auxDetail/opinionCard.jsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import StarRoundedIcon from "@mui/icons-material/StarRounded";
 import StarBorderRoundedIcon from "@mui/icons-material/StarBorderRounded";
 import Rating from "@mui/material/Rating";
 import { styled } from "@mui/material/styles";
+
 const StyledRating = styled(Rating)({
     "& .MuiRating-iconFilled": {
       color: "#1976d2",
@@ -17,16 +15,6 @@ const StyledRating = styled(Rating)({
       color: "#154bbf",
     },
   });
-
-
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
 
 export default function OpinionCard({detail}) {
   return (
