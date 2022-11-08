06baf93c040f2fa508fe9d1e75f0ec92b0b4aa68
juandalopez117 <juandalopez117@gmail.com>
2022-11-08 01:00:43 -0500

    ruta para obtener carrito - juan

--- api/src/routes/Carts.js
+++ api/src/routes/Carts.js
@@ -5,88 +5,110 @@ const router = Router()
 const { Cart, UserRegisted, Products, StateCarrito } = require('../db');
 
 router.post('/', async function(req, res){
-    const {idCart, user, products} = req.body
 
-        let subtotal = 0
-        for(let i = 1; i < products.length; i++ ){
-            subtotal = subtotal + products[i].unit_price * products[i].quantity
+    const { idCart, cart, subtotal, email} = req.body
+    const carrito = await Cart.findByPk(idCart);
+
+    const user = await UserRegisted.findOne({
+        where: {
+          email: email,
         }
+    });
 
-        let cart = await Cart.findByPk(idCart, {
-            include: {
-                model: Products
-            }
-        })
-
-        let usuario = await UserRegisted.findOne({
-            where: {email: user}
-        })
-
-        if(!cart){
-            let newCart = await Cart.create({
-                subtotal, 
-                isDeleted: false
-            })
-
-            await products.map(prod => {
-                newCart.addProducts(prod.id, {
-                    through: {
-                        precio: prod.unit_price,
-                        cantidad: prod.quantity,
-                        isDeleted: false
-                    }
-                })
-            })
-
-            await newCart.setStateCarrito(1)
-        
-            await UserRegisted.update({
-                cartId: newCart.id, 
-                }, {
-                where: {
-                    id: usuario.id
-                }
-            })
+    try{
+        if(!carrito){
+            const cartCreated = await Cart.create({
+                subtotal
+            });
             
-            const CartCreated = await Cart.findByPk(newCart.id, {
-                include: [
-                    {model: UserRegisted},
-                    {model: Products}
-                ]
-            })
-            res.send(CartCreated)
-        }
-        else{
+            cart.map(async (e) => {
+                await cartCreated.addProducts(e.id,
+                  {
+                    through: {
+                      precio: e.unit_price,
+                      cantidad: e.quantity
+                    }
+                  })
+            });
+
+            await cartCreated.setStateCarrito(4);
+
+            await user.addCart(cartCreated.id);
+
+
+        }else{
             await Cart.update({
                 subtotal
-            }, {
+              }, {
                 where: {
-                    id: cart.id
+                  id: idCart
                 }
-            })
+            });
+
+            await carrito.setProducts([]);
+
+            cart.map(async (e) => {
+                await carrito.addProducts(e.id,
+                  {
+                    through: {
+                      precio: e.unit_price,
+                      cantidad: e.quantity
+                    }
+                  })
+            });
+
+            await carrito.setStateCarrito(4);
+
+            user.addCart(carrito.id)
+        }
+    }catch(e){
+        console.log(e)
+    }
+    res.status(200).json('El carrito fue creado con exito!');
+})
+
+router.get('/', async function(req, res){
+    try{
+        const {email} = req.query
+        let result = await Cart.findAll({
+            attributes: ["id", "createdAt", "stateCarritoId"], 
+            include:[{
+                model: UserRegisted, 
+                attributes: ["id", "email", "isAdmin"]
+            }, 
+            {
+                model: Products, 
+                attributes: ["id", "name", "price", "description"]
+            }]
+        })
+
+        if(email){
+
+          let cartsEmail = []
+
+          result.map(el => {
+            if(el.userRegisted !== null && el.userRegisted.email.includes(email)){
+              cartsEmail.push(el)
+            }
+          })
+
+          cartsEmail.sort((a,b) => {
+            if( a.createdAt <  b.createdAt) return -1
+            else if(a.createdAt > b.createdAt) return 1
+            else return 0
+          })
+
+          res.status(200).send(cartsEmail[0])
         }
 
-        await cart.setProducts([])
-
-        await products.map(prod => {
-            cart.addProducts(prod.id, {
-                through: {
-                    precio: prod.unit_price,
-                    cantidad: prod.quantity,
-                    isDeleted: false
-                }
-            })
-        })
-
-        await cart.setStatusCarrito(1)
-
-        let cartUpdated = await Cart.findByPk(cart.id, {
-            include: [
-                {model: UserRegisted},
-                {model: Products}
-            ]
-        })
-        res.send(cartUpdated)
+        else{
+          res.send(result)
+        }
+       
+    }
+    catch(error){
+        console.log(error)
+    }
 })
 
 
