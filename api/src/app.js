4238ab45ee911e6d99a07204910a8156f612fc34
Luis Acosta <acosta1395@hotmail.com>
2023-03-31 12:37:35 -0300

    se especifica URL del front en api/src/app

--- api/src/app.js
+++ api/src/app.js
@@ -391,7 +391,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", `${FRONT_URL}`);
+  res.header("Access-Control-Allow-Origin", `https://henry-market.vercel.app/`);
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
